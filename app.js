9b5f1c30f862d0b394d56051c981c4eb011a8088
Florian Warin <florian.warin05@gmail.com>
2026-01-26 23:52:02 +0100

    Refactor event listeners for music and movie containers

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ button.addEventListener('click', () => console.log("Salut"));
 
 const musicContainer = document.getElementById("music-container");
 musicContainer.addEventListener('click', () => console.log("Musique"));
+
+const movieContainer = document.getElementById("movie-container");
+movieContainer.addEventListener('click', () => console.log("Films"));
