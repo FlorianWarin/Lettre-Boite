f0d72d3832ad80c916811e470df73835afa7e014
FlorianWarin <74072575+FlorianWarin@users.noreply.github.com>
2026-01-26 23:32:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("action-button");
+button.addEventListener('click', () => console.log("Salut"));
+
+const musicContainer = document.getElementById("music-container");
+musicContainer.addEventListener('click', () => console.log("Musique"));
